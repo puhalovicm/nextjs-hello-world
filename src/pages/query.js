211f011ce890e9e491d44aa76d1b65ef5a7a70b9
puhalovicm <mateo.puhalovic@gmail.com>
2024-06-18 15:33:10 +0200

    Local changes before commit

--- src/pages/query.js
+++ src/pages/query.js
@@ -1,21 +1,29 @@
+"use client";
+import React, { useState } from 'react';
+const QueryPage = () => {
+    const [currHostName, setCurrentHostname] = useState('');
+    const [currProtocol, setCurrentProtocol] = useState('');
+    const [currPort, setCurrentPort] = useState('');
+    const [currURL, setCurrentURL] = useState('');
 
-export default function QueryPage() {
-	const callAPI = async () => {
-		try {
-			const res = await fetch(
-				`https://v8zqtm.buildship.run/demo`
-			);
-			const data = await res.json();
-			console.log(data);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<button onClick={callAPI}>Make API Call</button>
-			</main>
-		</div>
-	);
-}
+    const handleGetURLDetails = () => {
+        setCurrentHostname(document.location.hostname);
+        setCurrentProtocol(document.location.protocol);
+        setCurrentPort(document.location.port || 'Default');
+        setCurrentURL(window.location.href);
+    };
+    return (
+        <div>
+            <h1>Using document.location</h1>
+            <button onClick={handleGetURLDetails}>Get URL Details</button>
+            <div>
+                <p>Current URL: {currURL}</p>
+                <p>Hostname: {currHostName}</p>
+                <p>Protocol: {currProtocol}</p>
+                <p>Port: {currPort}</p>
+            </div>
+        </div>
+    );
+};
+
+export default QueryPage;
