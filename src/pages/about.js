bba3430f251fdd6aa46fc3254caad1929ff4a26a
puhalovicm <mateo.puhalovic@gmail.com>
2024-06-18 12:02:08 +0200

    Show current domain

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,16 @@
-export default function About() {
-  return <div>About</div>;
-}
+"use client";
+import { useState } from 'react';
+const AboutPage = () => {
+    const [currentDomain, setCurrentDomain] = useState('');
+    const handleGetDomain = () => {
+        setCurrentDomain(window.location.origin);
+    };
+    return (
+        <div>
+            <h1>Using window.location</h1>
+            <button onClick={handleGetDomain}>Get Current Domain</button>
+            {currentDomain && <h2>Current URL Domain: {currentDomain}</h2>}
+        </div>
+    );
+};
+export default AboutPage;
