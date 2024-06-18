269193ea07e8c1df92f0933474c346596c0d702a
puhalovicm <mateo.puhalovic@gmail.com>
2024-06-18 15:39:49 +0200

    Local changes before commit

--- src/pages/query.js
+++ src/pages/query.js
@@ -1,29 +1,15 @@
-"use client";
-import React, { useState } from 'react';
-const QueryPage = () => {
-    const [currHostName, setCurrentHostname] = useState('');
-    const [currProtocol, setCurrentProtocol] = useState('');
-    const [currPort, setCurrentPort] = useState('');
-    const [currURL, setCurrentURL] = useState('');
+async function getArtist() {
+  const res = await fetch(`https://v8zqtm.buildship.run/demo`)
+  return res.json()
+}
+ 
+export default async function Page() {
+  // Initiate both requests in parallel
+  const artis = await getArtist()
 
-    const handleGetURLDetails = () => {
-        setCurrentHostname(document.location.hostname);
-        setCurrentProtocol(document.location.protocol);
-        setCurrentPort(document.location.port || 'Default');
-        setCurrentURL(window.location.href);
-    };
-    return (
-        <div>
-            <h1>Using document.location</h1>
-            <button onClick={handleGetURLDetails}>Get URL Details</button>
-            <div>
-                <p>Current URL: {currURL}</p>
-                <p>Hostname: {currHostName}</p>
-                <p>Protocol: {currProtocol}</p>
-                <p>Port: {currPort}</p>
-            </div>
-        </div>
-    );
-};
-
-export default QueryPage;
+  return (
+    <>
+      <h1>{artist.name}</h1>
+    </>
+  )
+}
