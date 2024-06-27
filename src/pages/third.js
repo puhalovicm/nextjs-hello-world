6e69020ea16de71cec7a8286eddb0317128a1011
puhalovicm <mateo.puhalovic@gmail.com>
2024-06-27 13:06:55 +0200

    Add server-side rendering

---
+++ src/pages/third.js
@@ -0,0 +1,21 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+
+export default function Page({ data }) {
+   return (
+        <div>
+          <h1>{data.title}</h1>
+          <p>{data.text1}</p>
+        </div>
+      );
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch('https://v8zqtm.buildship.run/demo')
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
