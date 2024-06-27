89575e3a8d6600525efb470da7952dd43eba82a1
puhalovicm <mateo.puhalovic@gmail.com>
2024-06-27 12:55:24 +0200

    Add client-side fetching

--- src/pages/second.js
+++ src/pages/second.js
@@ -1,16 +1,26 @@
 "use client";
-import React, { useState } from 'react';
-
-async function getArtist() {
-  const res = await fetch(`https://v8zqtm.buildship.run/demo`)
-  return res.json()
-}
+import React, { useState, useEffect } from 'react';
 
 export default function QueryPage() {
-  const arti = useState(getArtist());
-  return (
-    <div>
-      Hello World.{" "}
-    </div>
-  );
+   const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+      fetch('https://v8zqtm.buildship.run/demo')
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data)
+          setLoading(false)
+        })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
+
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.bio}</p>
+      </div>
+    );
 }
